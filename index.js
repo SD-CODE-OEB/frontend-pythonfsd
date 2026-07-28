e444801ed734f8aa75c52ba294b786b0cecef009
Syed Shoebuddin <ushoeb25@gmail.com>
2026-07-28 16:07:03 +0530

    feat:  update class 28-07-26

---
+++ index.js
@@ -0,0 +1,108 @@
+console.log("Hellow World!!");
+console.log("this is external file");
+
+// Data types in JavaScript
+
+//String
+//int / number
+//boolean
+//undefined
+//null
+
+// var name = "myname";
+
+let name, gender, favFood;
+
+console.log(name)
+
+let age18 = true;
+
+
+name = "myname"
+
+console.log(name)
+
+name = " yourname"
+
+console.log(name)
+
+const age = 8;
+
+console.log("default value", age)
+
+console.log("cahnged value", age)
+
+
+//conditionals
+if (age < 18) {
+  let numberNew = 20
+  if (numberNew > age) {
+    console.log(numberNew)
+  }
+} else {
+  console.log("not eligible")
+}
+
+
+if (age > 10) {
+
+} else if (age < 20) {
+
+} else {
+
+}
+
+const userObject = {
+  "name": "Shoeb",
+  "class": "12th",
+  "Address": "D-mart",
+  address: "HQ buliding"
+}
+
+console.log(userObject["address"], userObject['Address'], userObject.class);
+
+
+const arr = ["1", "2", true, 56]
+console.log(arr[0], arr[1], arr[2], arr[3], arr.length)
+
+let t = 0;
+while (t < arr.length) {
+  console.log(arr[t])
+  t = t + 1
+}
+
+for (let i = 0; i <= 15; i = i + 1) {
+  console.log(i)
+}
+
+let i = 0;
+do {
+  // Code block to be executed
+  console.log(i)
+  i = i + 1
+} while (i < 5);
+
+
+
+
+
+
+
+
+//loops
+
+
+// const name = "tellyourname"
+
+
+
+//data types of python
+// int
+//float
+//boolean
+//string
+//none
+
+
+
+
---
+++ index1.js
@@ -0,0 +1,4 @@
+const myarr = [12, 45, 8, 67, 23];
+const sortedArray = myarr.sort((a, b) => a + b);
+console.log(sortedArray[sortedArray.length - 1])
+
